d0267527460a3dbad810bad4e78ee0a4189cded6
Iskren Chernev <iskren.chernev@gmail.com>
2017-09-24 15:08:26 +0300

    [09] shouldInterceptRequest

--- 09.WebView/01.Simple/app/src/main/assets/script.js
+++ 09.WebView/01.Simple/app/src/main/assets/script.js
@@ -3,8 +3,8 @@ document.write("I won!");
 window.addEventListener("message", function (event) {
     console.log("YES");
     alert("foo");
-    document.write("event.origin " + event.origin + "\n");
-    document.write("event.data " + event.data + "\n");
+    document.write("event.origin " + JSON.stringify(event.origin) + "\n");
+    document.write("event.data " + JSON.stringify(event.data) + "\n");
 }, false);
 
 injectedObject.scriptLoaded();
--- 09.WebView/01.Simple/app/src/main/java/com/example/iskren/webview/simple/WebActivity.java
+++ 09.WebView/01.Simple/app/src/main/java/com/example/iskren/webview/simple/WebActivity.java
@@ -1,16 +1,21 @@
 package com.example.iskren.webview.simple;
 
+import android.annotation.TargetApi;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Handler;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.webkit.ConsoleMessage;
 import android.webkit.JavascriptInterface;
+import android.webkit.ValueCallback;
 import android.webkit.WebChromeClient;
 import android.webkit.WebMessage;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebResourceResponse;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
@@ -20,6 +25,7 @@ import org.json.JSONException;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Map;
 
 public class WebActivity extends AppCompatActivity {
 
@@ -56,7 +62,51 @@ public class WebActivity extends AppCompatActivity {
         webView = (WebView) findViewById(R.id.webview);
 
         // Otherwise the loadUrl below just spawns a real browser
-        webView.setWebViewClient(new WebViewClient());
+        webView.setWebViewClient(new WebViewClient() {
+
+            @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+            @Override
+            public WebResourceResponse shouldInterceptRequest (
+                    WebView view, WebResourceRequest request) {
+                WebResourceResponse res = handleInterceptRequest(view, request.getUrl());
+                if (res == null) {
+                    return super.shouldInterceptRequest(view, request);
+                } else {
+                    return res;
+                }
+            }
+
+            @SuppressWarnings("deprecation")
+            @Override
+            public WebResourceResponse shouldInterceptRequest(WebView view, String x) {
+                WebResourceResponse res = handleInterceptRequest(view, Uri.parse(x));
+                if (res == null) {
+                    return super.shouldInterceptRequest(view, x);
+                } else {
+                    return res;
+                }
+            }
+
+            private WebResourceResponse handleInterceptRequest(WebView view, Uri uri) {
+                String req = uri.toString();
+                if (req.endsWith("script.js")) {
+                    try {
+                        InputStream is = getAssets().open(req.substring(baseUrl.length()));
+                        return new WebResourceResponse("application/javascript", "utf-8", is);
+                    } catch (IOException e) {
+                        WebResourceResponse res = new WebResourceResponse(
+                                "application/javascript", "utf-8", null);
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                            res.setStatusCodeAndReasonPhrase(404, "Not Found");
+                        }
+                        return res;
+                    }
+                } else {
+                    return null;
+                }
+            }
+
+        });
         webView.setWebChromeClient(new WebChromeClient() {
             @Override
             public boolean onConsoleMessage(ConsoleMessage message) {
@@ -77,7 +127,14 @@ public class WebActivity extends AppCompatActivity {
 //
 //        webView.loadData("<h1 id='x'>hi</h1>", "text/html", null);
 //
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            webView.evaluateJavascript("[1,2,true, /ala/]", new ValueCallback<String>() {
+                @Override
+                public void onReceiveValue(String value) {
+                    Log.i("TAG", "Got back " + value);
+                }
+            });
+        }
 //            webView.evaluateJavascript("console.log(5)", null);
 //            webView.evaluateJavascript("console.log(injectedObject.join(['1', '2'], 'xxx'))", null); // 1xxx2
 //            webView.evaluateJavascript("console.log(injectedObject.join([1, '2'], 'xxx'))", null); // nullxxx2
@@ -87,7 +144,7 @@ public class WebActivity extends AppCompatActivity {
 //            //webView.evaluateJavascript("document.write(injectedObject.toString())", null);
 //        }
 //        // webView.loadUrl("javascript:console.log(injectedObject.toString())");
-        baseUrl = "file:///android_asset/index.html";
+        baseUrl = "https://iskren.info/"; // "file:///android_asset/index.html";
         webView.loadDataWithBaseURL(
                 baseUrl,
                 readAssetFile(this, "index.html"),
@@ -95,12 +152,21 @@ public class WebActivity extends AppCompatActivity {
                 "utf-8",
                 null);
 
+        final Handler h = new Handler(this.getMainLooper());
+        final Runnable r = new Runnable() {
+            @Override
+            public void run() {
+                webView.loadUrl("javascript:document.write(\"win\")");
+                h.postDelayed(this, 2000);
+            }
+        };
+        h.postDelayed(r, 1000);
     }
 
     private void doPostWebMessage() {
         Log.i("TAG", "Now we can post!!");
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            webView.postWebMessage(new WebMessage("You can haz data"), Uri.parse(""));
+            webView.postWebMessage(new WebMessage("You can haz data"), Uri.parse(baseUrl));
         }
     }
 
