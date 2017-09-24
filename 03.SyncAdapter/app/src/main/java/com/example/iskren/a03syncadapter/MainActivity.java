fdde2d97ada8a2714e6588124eea9e0f93ce25e8
Iskren Chernev <iskren.chernev@gmail.com>
2017-09-24 18:04:38 +0300

    [03] Add manual sync option

--- 03.SyncAdapter/app/src/main/java/com/example/iskren/a03syncadapter/MainActivity.java
+++ 03.SyncAdapter/app/src/main/java/com/example/iskren/a03syncadapter/MainActivity.java
@@ -3,24 +3,29 @@ package com.example.iskren.a03syncadapter;
 import android.accounts.Account;
 import android.accounts.AccountManager;
 import android.content.ContentProvider;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "SampleSyncAdapter";
 
     // The authority for the sync adapter's content provider
-    // Make sure it matches AndroidManifest <provider android:authorities="XXX">
+    // Make sure it matches AndroidManifest <provider android:authorities="XXX"> and @xml/syncadapter
     public static final String AUTHORITY = "com.example.iskren.a03syncadapter.provider";
     // An account type, in the form of a domain name
     // Make sure it matches @xml/authenticator android:accountType="XXX"
     public static final String ACCOUNT_TYPE = "iskren.example.com";
     // The account name
     public static final String ACCOUNT_NAME = "noname";
+    // 1 hour, this is the minimum
+    private static final long SYNC_FREQUENCY = 60L * 60L;
 
     private static boolean accountSetupFailed;
 
@@ -30,8 +35,48 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // ensure account is created
-        getSyncAccount(this);
+        Account account = getSyncAccount(this);
+        if (account == null) {
+            finish();
+        }
+
+        setPeriodicSync();
+
+        Button syncButton = (Button) findViewById(R.id.sync_button);
+
+        syncButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                forceSync();
+            }
+        });
     }
+
+    private void setPeriodicSync() {
+        Account account = getSyncAccount(this);
+
+        // Add a periodic sync, note that this overwrites any previous periodic sync settings
+        ContentResolver.addPeriodicSync(
+                account,
+                AUTHORITY,
+                Bundle.EMPTY,
+                SYNC_FREQUENCY);
+    }
+
+    private void forceSync() {
+        Log.i(TAG, "forcing sync");
+        Account account = getSyncAccount(this);
+
+        // Pass the settings flags by inserting them in a bundle
+        Bundle settingsBundle = new Bundle();
+        settingsBundle.putBoolean(
+                ContentResolver.SYNC_EXTRAS_MANUAL, true);
+        settingsBundle.putBoolean(
+                ContentResolver.SYNC_EXTRAS_EXPEDITED, true);
+
+        ContentResolver.requestSync(account, AUTHORITY, settingsBundle);
+    }
+
 
     @Nullable
     public static Account getSyncAccount(Context context) {
