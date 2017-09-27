ba43ec6883f4bef5a15db79c392d5c5a2ea3e67b
Iskren Chernev <iskren.chernev@gmail.com>
2017-09-27 15:26:32 +0300

    Looper thread example

--- 07.Threading/app/src/main/java/com/example/iskren/a07threading/MainActivity.java
+++ 07.Threading/app/src/main/java/com/example/iskren/a07threading/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.media.Image;
 import android.os.AsyncTask;
 import android.os.Handler;
+import android.os.Looper;
 import android.os.Message;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
@@ -17,6 +18,8 @@ public class MainActivity extends AppCompatActivity {
     public static final int ADD_MSG = 0;
     public static final int MUL_MSG = 1;
 
+    LooperThread [] lt;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,11 +32,20 @@ public class MainActivity extends AppCompatActivity {
         startService(intent);
 
         Handler h = new MyHandler();
-        h.sendMessage(h.obtainMessage(ADD_MSG, 2, 2));
 
+        // h.sendMessage(h.obtainMessage(ADD_MSG, 2, 2));
+
+        /*
         for (int i = 0; i < 5; ++i) {
             new MyAsyncTask(h).executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, i);
+        }*/
+
+        lt = new LooperThread[3];
+        for (int i = 0; i < 3; ++i) {
+            lt[i] = new LooperThread(h);
+            lt[i].start();
         }
+        //lt[0].mHandler.post(new TellNext(0, 0));
 
         /*
         (new Handler()).postDelayed(new Runnable() {
@@ -54,9 +66,39 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    class TellNext implements Runnable {
+        private int threadId;
+        private int level;
+
+        public TellNext(int threadId, int level) {
+
+            this.threadId = threadId;
+            this.level = level;
+        }
+
+        @Override
+        public void run() {
+
+            if (level == 5) {
+                return;
+            }
+            Log.i(TAG, String.format("%s level %d: process %d thread %s",
+                    "Looper" + threadId, level, android.os.Process.myPid(), android.os.Process.myTid()));
+
+            int nxt = (threadId + 1) % 3;
+            MainActivity.this.lt[nxt].mHandler.post(new TellNext(nxt, nxt == 0 ? level + 1 : level));
+        }
+    }
+
     class MyHandler extends Handler {
+        int total = 0;
         @Override
         public void handleMessage(Message msg) {
+            total += 1;
+            if (total == 3) {
+                MainActivity.this.lt[0].mHandler.post(new TellNext(0, 0));
+            }
+            /*
             switch (msg.what) {
                 case ADD_MSG:
                     Log.i(TAG, String.format("%d + %d = %d", msg.arg1, msg.arg2, msg.arg1 + msg.arg2));
@@ -66,7 +108,7 @@ public class MainActivity extends AppCompatActivity {
                     break;
                 default:
                     Log.e(TAG, "unknown");
-            }
+            }*/
         }
     }
 
@@ -103,6 +145,23 @@ public class MainActivity extends AppCompatActivity {
         protected void onPostExecute(Void aVoid) {
             Log.i(TAG, String.format("%s back on main[%d] ready: process %d thread %s",
                     getClass().getName(), id, android.os.Process.myPid(), android.os.Process.myTid()));
+        }
+    }
+
+    class LooperThread extends Thread {
+        public Handler mHandler;
+        private Handler notif;
+
+        public LooperThread(Handler notif) {
+            this.notif = notif;
+        }
+
+        public void run() {
+            Looper.prepare();
+            notif.sendMessage(notif.obtainMessage());
+            mHandler = new Handler();
+
+            Looper.loop();
         }
     }
 }
