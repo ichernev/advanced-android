c4c4583336dd926e20898b348e7dcaa2b5bd53a9
Iskren Chernev <iskren.chernev@gmail.com>
2017-09-27 16:17:36 +0300

    LocalService (extending Binder) example

--- 07.Threading/app/src/main/java/com/example/iskren/a07threading/MainActivity.java
+++ 07.Threading/app/src/main/java/com/example/iskren/a07threading/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.iskren.a07threading;
 
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.media.Image;
 import android.os.AsyncTask;
+import android.os.DeadObjectException;
 import android.os.Handler;
+import android.os.IBinder;
 import android.os.Looper;
 import android.os.Message;
 import android.provider.MediaStore;
@@ -14,11 +19,10 @@ import android.util.Log;
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "SampleThread";
+    private static final int SERVICE_BOUND_MSG = 0;
 
-    public static final int ADD_MSG = 0;
-    public static final int MUL_MSG = 1;
-
-    LooperThread [] lt;
+    private LocalService mService;
+    private boolean mBound;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,141 +31,58 @@ public class MainActivity extends AppCompatActivity {
 
         Log.i(TAG, String.format("%s FIRST: process %d thread %s",
                 getClass().getName(), android.os.Process.myPid(), android.os.Process.myTid()));
-
-        Intent intent = new Intent(this, MainService.class);
-        startService(intent);
-
-        Handler h = new MyHandler();
-
-        // h.sendMessage(h.obtainMessage(ADD_MSG, 2, 2));
-
-        /*
-        for (int i = 0; i < 5; ++i) {
-            new MyAsyncTask(h).executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, i);
-        }*/
-
-        lt = new LooperThread[3];
-        for (int i = 0; i < 3; ++i) {
-            lt[i] = new LooperThread(h);
-            lt[i].start();
-        }
-        //lt[0].mHandler.post(new TellNext(0, 0));
-
-        /*
-        (new Handler()).postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent intent = new Intent(MainActivity.this, MainService.class);
-                Log.i(TAG, String.format("%s SECOND: process %d thread %s",
-                        getClass().getName(), android.os.Process.myPid(), android.os.Process.myTid()));
-                startService(intent);
-            }
-        }, 2 * Constants.SECOND);
-        */
-
-        /*
-        final int x = 5;
-        final VarHolder<Integer> y = new VarHolder<>(5);
-        */
-
     }
 
-    class TellNext implements Runnable {
-        private int threadId;
-        private int level;
+    protected void onStart() {
+        super.onStart();
 
-        public TellNext(int threadId, int level) {
+        Intent intent = new Intent(this, LocalService.class);
+        bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
+    }
 
-            this.threadId = threadId;
-            this.level = level;
+    protected void useService() {
+        Log.i(TAG, "RND " + mService.getRandomNumber());
+    }
+
+    protected void onStop() {
+        super.onStop();
+
+        if (mBound) {
+            unbindService(mConnection);
+            mBound = false;
+        }
+    }
+
+    /** Defines callbacks for service binding, passed to bindService() */
+    private ServiceConnection mConnection = new ServiceConnection() {
+
+        @Override
+        public void onServiceConnected(ComponentName className,
+                                       IBinder service) {
+            // We've bound to LocalService, cast the IBinder and get LocalService instance
+            LocalService.LocalBinder binder = (LocalService.LocalBinder) service;
+            mService = binder.getService();
+            mBound = true;
+
+            handler.sendMessage(handler.obtainMessage(SERVICE_BOUND_MSG));
         }
 
         @Override
-        public void run() {
+        public void onServiceDisconnected(ComponentName arg0) {
+            mBound = false;
+        }
+    };
 
-            if (level == 5) {
-                return;
-            }
-            Log.i(TAG, String.format("%s level %d: process %d thread %s",
-                    "Looper" + threadId, level, android.os.Process.myPid(), android.os.Process.myTid()));
-
-            int nxt = (threadId + 1) % 3;
-            MainActivity.this.lt[nxt].mHandler.post(new TellNext(nxt, nxt == 0 ? level + 1 : level));
+    private void handleMessage(Message msg) {
+        if (msg.what == SERVICE_BOUND_MSG) {
+            useService();
         }
     }
 
-    class MyHandler extends Handler {
-        int total = 0;
+    private Handler handler = new Handler() {
         @Override
         public void handleMessage(Message msg) {
-            total += 1;
-            if (total == 3) {
-                MainActivity.this.lt[0].mHandler.post(new TellNext(0, 0));
-            }
-            /*
-            switch (msg.what) {
-                case ADD_MSG:
-                    Log.i(TAG, String.format("%d + %d = %d", msg.arg1, msg.arg2, msg.arg1 + msg.arg2));
-                    break;
-                case MUL_MSG:
-                    Log.i(TAG, String.format("%d * %d = %d", msg.arg1, msg.arg2, msg.arg1 * msg.arg2));
-                    break;
-                default:
-                    Log.e(TAG, "unknown");
-            }*/
+            MainActivity.this.handleMessage(msg);
         }
-    }
-
-    class MyAsyncTask extends AsyncTask<Integer, Void, Void> {
-
-        private Handler h;
-        private int id;
-
-        public MyAsyncTask(Handler h) {
-            this.h = h;
-        }
-
-        @Override
-        protected Void doInBackground(Integer... params) {
-            id = params[0];
-            h.sendMessage(h.obtainMessage(ADD_MSG, id, id));
-
-            Log.i(TAG, String.format("%s worker[%d]: process %d thread %s",
-                    getClass().getName(), id, android.os.Process.myPid(), android.os.Process.myTid()));
-            try {
-                Thread.sleep(1 * Constants.SECOND);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-            h.sendMessage(h.obtainMessage(MUL_MSG, id, id));
-
-            Log.i(TAG, String.format("%s worker[%d] ready: process %d thread %s",
-                    getClass().getName(), id, android.os.Process.myPid(), android.os.Process.myTid()));
-
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(Void aVoid) {
-            Log.i(TAG, String.format("%s back on main[%d] ready: process %d thread %s",
-                    getClass().getName(), id, android.os.Process.myPid(), android.os.Process.myTid()));
-        }
-    }
-
-    class LooperThread extends Thread {
-        public Handler mHandler;
-        private Handler notif;
-
-        public LooperThread(Handler notif) {
-            this.notif = notif;
-        }
-
-        public void run() {
-            Looper.prepare();
-            notif.sendMessage(notif.obtainMessage());
-            mHandler = new Handler();
-
-            Looper.loop();
-        }
-    }
+    };
 }
